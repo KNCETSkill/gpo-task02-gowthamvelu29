01de20e2e0f53d63de3731ba01e5cbc2c0dcf642
Gowtham <gowthamvelu2909@gmail.com>
2026-01-19 16:14:34 +0530

    Fix attendance threshold condition for excellence

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -3,7 +3,7 @@ public class AttendanceStatusEvaluator{
   public static void main(String[] args){
     Scanner scanner=new Scanner(System.in);
     int attendance=scanner.nextInt();
-    if(attendance>=85){
+    if(attendance>85){
       System.out.println("Excellent");
     }
     else if(attendance>=60){
